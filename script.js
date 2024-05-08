7259d353c5a8621d99ad9c40613ce59110966efd
DCKAP <gunasrigdckap@gmail.com>
2024-05-08 14:33:27 +0530

    audio

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll(".card");
-
+    var audio = document.getElementById('myAudio');
     cards.forEach((card) => {
         card.addEventListener("click", function () {
             card.classList.toggle("flipped");
+            audio.play()
         });
     });
 });
